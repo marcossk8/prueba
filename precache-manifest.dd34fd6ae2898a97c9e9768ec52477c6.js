d7498fa3c4c102e05413ec90bae445f49ac313b3
MarcosAlmendros <malmendros@sooft.com.ar>
2020-10-10 16:37:05 -0300

    Updates

---
+++ precache-manifest.dd34fd6ae2898a97c9e9768ec52477c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a472d8a4b1ceeafb78b24d6d87558d50",
+    "url": "/prueba/index.html"
+  },
+  {
+    "revision": "05f74d860cef4018097c",
+    "url": "/prueba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76b6b4257746597d704c",
+    "url": "/prueba/static/js/2.8eb8d0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba/static/js/2.8eb8d0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f74d860cef4018097c",
+    "url": "/prueba/static/js/main.14c445e3.chunk.js"
+  },
+  {
+    "revision": "ef43a5002b1ffe0c70f0",
+    "url": "/prueba/static/js/runtime-main.d2f08c97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba/static/media/logo.5d5d9eef.svg"
+  }
+]);
